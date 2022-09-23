0463fd35f2d3adfe4c9b1d735209e066f5881d67
U61vashka <botyrbojey@gmail.com>
2022-09-24 00:42:53 +0600

    Move deleteMessage to the default method

--- core/src/main/java/com/ubivashka/vk/api/providers/VkApiProvider.java
+++ core/src/main/java/com/ubivashka/vk/api/providers/VkApiProvider.java
@@ -1,8 +1,12 @@
 package com.ubivashka.vk.api.providers;
 
+import java.util.Collections;
+
 import com.ubivashka.vk.api.parsers.LongpoolEventParser;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.GroupActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.messages.Message;
 
 public interface VkApiProvider {
@@ -12,5 +16,18 @@ public interface VkApiProvider {
 
 	LongpoolEventParser getLongpoolParser();
 
-	boolean deleteMessage(Message message);
+	/**
+	 * Deprecated because it must be another class, like: VKHelper or VkMethodHelper
+	 */
+	@Deprecated
+	default boolean deleteMessage(Message message){
+		try {
+			getVkApiClient().messages().delete(getActor()).messageIds(Collections.singletonList(message.getId())).deleteForAll(true)
+					.execute();
+			return true;
+		} catch (ApiException | ClientException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
